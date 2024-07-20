8505d81839f2185d8b56933dc59f8384745572fe
tookuyam <tookuyam@42.student.fr>
2024-07-20 12:48:22 +0900

    update: install script

--- install.sh
+++ install.sh
@@ -9,13 +9,19 @@ cd "$INSTALLDIR" || exit 1
 INSTALLDIR="$(pwd)"
 PROJECT_PATH="$INSTALLDIR/$REPO_NAME"
 
-git clone git@github.com:PalmNeko/"$REPO_NAME".git --depth 1 -b "$BRANCH_NAME" "$REPO_NAME"
+test -d "$PROJECT_PATH" || git clone git@github.com:PalmNeko/"$REPO_NAME".git --depth 1 -b "$BRANCH_NAME" "$REPO_NAME"
 
-if [ -f "~/.zshrc" ]; then
-	echo "alias paco='$PROJECT_PATH/test.sh'" >> "~/.zshrc"
-	echo "alias francinette='$PROJECT_PATH/test.sh'" >> "~/.zshrc"
+TEST_SCRIPT="$PROJECT_PATH/test.sh"
+chmod u+x "$TEST_SCRIPT"
+
+APPEND_TEXT="
+# append from $REPO_NAME
+alias paco='$TEST_SCRIPT'
+alias francinette='$TEST_SCRIPT'
+"
+if [ -f ~/.zshrc ]; then
+	echo "$APPEND_TEXT" >> ~/.zshrc
 fi
-if [ -f "~/.bashrc" ]; then
-	echo "alias paco='$PROJECT_PATH/test.sh'" >> "~/.zshrc"
-	echo "alias francinette='$PROJECT_PATH/test.sh'" >> "~/.zshrc"
+if [ -f ~/.bashrc ]; then
+	echo "$APPEND_TEXT" >> ~/.bashrc
 fi
